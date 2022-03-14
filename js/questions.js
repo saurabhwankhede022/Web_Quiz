9197f32453209cf93533b767eb3a2e418e600aab
saurabhwankhede022 <saurabhwankhede022@gmail.com>
2022-03-14 20:59:40 +0530

    First commit

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "High Text Markup Language",
+      "Hyper Text Markup Language",
+      "Hyper Tabular Markup Language",
+      "None of these"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which method of the Component class is used to set the position and size of a component in JSP?",
+    answer: "setBounds()",
+    options: [
+      "setSize()",
+      "setPosition()",
+      "setBounds()",
+      "setPositionSize()"
+    ]
+  },
+    {
+    numb: 3,
+    question: "HTML is a subset of",
+    answer: "SGML",
+    options: [
+      "SGML",
+      "SGMT",
+      "SGMD",
+      "SGMJ"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Any part of the graphic that is not included in another hot zone is considered to be part of",
+    answer: "Default",
+    options: [
+      "Polygon",
+      "Point",
+      "Rect-JS",
+      "Default"
+    ]
+  },
+    {
+    numb: 5,
+    question: "While working on a JavaScript project, in your JavaScript application, which function would you use to send messages to users requesting for text input?",
+    answer: "Prompt()",
+    options: [
+      "Prompt()",
+      "Alert()",
+      "GetInput()",
+      "Display()"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
